8dc3692969b765f53a01b64e4be3786c4a387f98
Ihar Yatsevich <igor.nikolaevich.96@gmail.com>
2026-03-29 22:55:12 +0200

    [Docs] Add author info headers to install.sh and uninstall.sh (#22)

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Author: Ihar Yatsevich <igor.nikolaevich.96@gmail.com>
+# Date: 27.03.2026
+# License: GPL-3.0
+# Fork: https://github.com/IgrikXD/rpitx-ui
+# RF transmitter for Raspberry Pi with improved UI functionality, built with CMake.
+
 # rpitx-ui package version
 PACKAGE_VERSION='1.4'
 
