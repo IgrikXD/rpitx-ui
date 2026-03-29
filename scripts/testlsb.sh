27d5062414a8de39f1cb9e149a9a273bf2cd8a6e
IgrikXD <igor.nikolaevich.96@gmail.com>
2026-03-29 22:32:27 +0200

    [Docs] Add author info headers to testusb.sh and testlsb.sh

--- scripts/testlsb.sh
+++ scripts/testlsb.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Author: Ihar Yatsevich <igor.nikolaevich.96@gmail.com>
+# Date: 27.03.2026
+# License: GPL-3.0
+# Fork: https://github.com/IgrikXD/rpitx-ui
+# RF transmitter for Raspberry Pi with improved UI functionality, built with CMake.
+
 (while true; do cat "$2"; done) | pissb -l \
   | sudo sendiq -i /dev/stdin -s 48000 -f "$1" -t float
