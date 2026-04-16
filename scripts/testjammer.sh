20a1c4e9099b5b823ede023dd2cce6f3ae91d19e
IgrikXD <igor.nikolaevich.96@gmail.com>
2026-04-16 20:26:20 +0200

    Add testjammer script for RF jamming functionality

---
+++ scripts/testjammer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Author: Ihar Yatsevich <igor.nikolaevich.96@gmail.com>
+# Date: 14.04.2026
+# License: GPL-3.0
+# Fork: https://github.com/IgrikXD/rpitx-ui
+# RF transmitter for Raspberry Pi with improved UI functionality, built with CMake.
+
+# Usage: testjammer.sh <freq_Hz> <bandwidth_Hz> <mode> [tones]
+#   <mode>    noise | sweep | multitone
+#   [tones]   Tone count for multitone mode (default 8, ignored in other modes)
+TONES_FLAG=()
+if [ "$3" = "multitone" ]; then
+	TONES_FLAG=(-t "${4:-8}")
+fi
+
+sudo pijammer "$1" "$2" -m "$3" "${TONES_FLAG[@]}"
