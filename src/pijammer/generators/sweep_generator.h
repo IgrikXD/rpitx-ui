180166c62e0444e86ec32846a7f647d72799aa9e
IgrikXD <igor.nikolaevich.96@gmail.com>
2026-04-16 11:55:44 +0200

    Add SweepGenerator class for linear sawtooth frequency sweeping

---
+++ src/pijammer/generators/sweep_generator.h
@@ -0,0 +1,43 @@
+/**
+ * @file sweep_generator.h
+ * @brief Linear sawtooth sweep generator.
+ *
+ * @author Ihar Yatsevich <igor.nikolaevich.96@gmail.com>
+ * @date 15.04.2026
+ * @copyright GPL-3.0
+ * @see https://github.com/IgrikXD/rpitx-ui
+ * @note RF transmitter for Raspberry Pi with improved UI functionality, built with CMake.
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#include "jammer_generator.h"
+#include "sawtooth.h"
+
+/**
+ * @brief Thin wrapper around the Sawtooth primitive for frequency sweeping.
+ *
+ * Produces a linear ramp spanning [-bandwidth/2, +bandwidth/2] and wrapping at
+ * the positive peak. Fully deterministic - no RNG, no hold state needed.
+ */
+class SweepGenerator : public JammerGenerator {
+public:
+    /**
+     * @brief Construct a sweep generator for the given RF bandwidth and DMA sample rate.
+     * @param bandwidth Target RF bandwidth in Hz (amplitude of the ramp is bandwidth / 2).
+     * @param sampleRate DMA sample rate in Hz.
+     */
+    SweepGenerator(float bandwidth, uint32_t sampleRate);
+
+    [[nodiscard]] float nextSample() override;
+
+private:
+    /**
+     * @brief Number of full sweep cycles per second.
+     */
+    static constexpr float RATE_HZ{1'000.0F};
+
+    Sawtooth sawtooth_;  ///< Underlying ramp oscillator.
+};
