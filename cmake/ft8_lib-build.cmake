b4e4bbb7cf2e5f2ef95f93df88b3dc9fb8168850
IgrikXD <igor.nikolaevich.96@gmail.com>
2026-04-23 20:35:42 +0200

    Refactor ft8_lib build configuration to simplify directory creation and improve installation commands

--- cmake/ft8_lib-build.cmake
+++ cmake/ft8_lib-build.cmake
@@ -12,8 +12,7 @@ set(FT8_LIB_INSTALL_DIR "${THIRD_PARTY_INSTALL_DIR}/ft8_lib")
 set(FT8_LIB_LIBRARY "${FT8_LIB_INSTALL_DIR}/lib/libft8.a")
 
 file(MAKE_DIRECTORY
-    "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/common"
-    "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/ft8"
+    "${FT8_LIB_INSTALL_DIR}/include/ft8_lib"
     "${FT8_LIB_INSTALL_DIR}/lib"
 )
 
@@ -23,21 +22,17 @@ ExternalProject_Add(ft8_lib
     GIT_TAG "91f2e648c8755d717177586675262310862bc0a8"
     UPDATE_DISCONNECTED TRUE
     SOURCE_DIR "${THIRD_PARTY_SOURCE_DIR}/ft8_lib"
-    BINARY_DIR "${THIRD_PARTY_BUILD_DIR}/ft8_lib"
+    BUILD_IN_SOURCE TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND "${MAKE_EXECUTABLE}" -C "<SOURCE_DIR>" all
     INSTALL_COMMAND
-        "${CMAKE_COMMAND}" -E make_directory
-            "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/common"
-            "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/ft8"
-            "${FT8_LIB_INSTALL_DIR}/lib"
-        COMMAND sh -c
-            "cp \"<SOURCE_DIR>\"/common/*.h \"${FT8_LIB_INSTALL_DIR}/include/ft8_lib/common/\""
-        COMMAND sh -c
-            "cp \"<SOURCE_DIR>\"/ft8/*.h \"${FT8_LIB_INSTALL_DIR}/include/ft8_lib/ft8/\""
+        "${CMAKE_COMMAND}" -E copy_directory
+            "<SOURCE_DIR>/common" "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/common"
+        COMMAND "${CMAKE_COMMAND}" -E copy_directory
+            "<SOURCE_DIR>/ft8" "${FT8_LIB_INSTALL_DIR}/include/ft8_lib/ft8"
         COMMAND "${CMAKE_COMMAND}" -E rm -f "${FT8_LIB_LIBRARY}"
         COMMAND sh -c
-            "cd \"<SOURCE_DIR>\" && \"${CMAKE_AR}\" rc \"${FT8_LIB_LIBRARY}\" common/*.o fft/*.o ft8/*.o"
+            "\"${CMAKE_AR}\" rc \"${FT8_LIB_LIBRARY}\" \"<SOURCE_DIR>\"/common/*.o \"<SOURCE_DIR>\"/fft/*.o \"<SOURCE_DIR>\"/ft8/*.o"
         COMMAND "${CMAKE_RANLIB}" "${FT8_LIB_LIBRARY}"
     INSTALL_BYPRODUCTS "${FT8_LIB_LIBRARY}"
 )
